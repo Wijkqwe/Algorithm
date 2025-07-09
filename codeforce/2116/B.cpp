82ab0eb76546b3fc466984008c12d07c68375024
Wijkqwe <WZYqwe2004@outlook.com>
2025-07-09 21:59:07 +0800

    2116

---
+++ codeforce/2116/B.cpp
@@ -0,0 +1,421 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define endl "\n"
+#define fast                                                                                                           \
+    ios::sync_with_stdio(false);                                                                                       \
+    ios_base::sync_with_stdio(0);                                                                                      \
+    cin.tie(0);                                                                                                        \
+    cout.tie(0);
+#define gc() getchar()
+#define MOD 100000000
+#define one void(cout << "-1\n")
+#define pb(c) push_back(c)
+#define pf(c) push_front(c)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define rint register int
+#define vi vector<int>
+#define vll vector<long long>
+#define fi first
+#define se second
+#define all(c) (c).begin(), (c).end()
+typedef long long ll;
+typedef unsigned long long ull;
+
+namespace Fread
+{
+const int SIZE = 1 << 16;
+char buf[SIZE], *S, *T;
+inline char getchar()
+{
+    if (S == T)
+    {
+        T = (S = buf) + fread(buf, 1, SIZE, stdin);
+        if (S == T)
+            return '\n';
+    }
+    return *S++;
+}
+}  // namespace Fread
+using namespace Fread;
+namespace Fwrite
+{
+const int SIZE = 1 << 16;
+char buf[SIZE], *S = buf, *T = buf + SIZE;
+inline void flush()
+{
+    fwrite(buf, 1, S - buf, stdout);
+    S = buf;
+}
+inline void putchar(char c)
+{
+    *S++ = c;
+    if (S == T)
+        flush();
+}
+struct NTR
+{
+        ~NTR() { flush(); }
+} ztr;
+}  // namespace Fwrite
+using namespace Fwrite;
+#define getchar Fread::getchar
+#define putchar Fwrite::putchar
+namespace Fastio
+{
+struct Reader
+{
+        template <typename T>
+        Reader& operator>>(T& x)
+        {
+            x = 0;
+            short f = 1;
+            char c = getchar();
+            while (c < '0' || c > '9')
+            {
+                if (c == '-')
+                    f *= -1;
+                c = getchar();
+            }
+            while (c >= '0' && c <= '9')
+                x = (x << 3) + (x << 1) + (c ^ 48), c = getchar();
+            x *= f;
+            return *this;
+        }
+        Reader& operator>>(double& x)
+        {
+            x = 0;
+            double t = 0;
+            short f = 1, s = 0;
+            char c = getchar();
+            while ((c < '0' || c > '9') && c != '.')
+            {
+                if (c == '-')
+                    f *= -1;
+                c = getchar();
+            }
+            while (c >= '0' && c <= '9' && c != '.')
+                x = x * 10 + (c ^ 48), c = getchar();
+            if (c == '.')
+                c = getchar();
+            else
+            {
+                x *= f;
+                return *this;
+            }
+            while (c >= '0' && c <= '9')
+                t = t * 10 + (c ^ 48), s++, c = getchar();
+            while (s--)
+                t /= 10.0;
+            x = (x + t) * f;
+            return *this;
+        }
+        Reader& operator>>(long double& x)
+        {
+            x = 0;
+            long double t = 0;
+            short f = 1, s = 0;
+            char c = getchar();
+            while ((c < '0' || c > '9') && c != '.')
+            {
+                if (c == '-')
+                    f *= -1;
+                c = getchar();
+            }
+            while (c >= '0' && c <= '9' && c != '.')
+                x = x * 10 + (c ^ 48), c = getchar();
+            if (c == '.')
+                c = getchar();
+            else
+            {
+                x *= f;
+                return *this;
+            }
+            while (c >= '0' && c <= '9')
+                t = t * 10 + (c ^ 48), s++, c = getchar();
+            while (s--)
+                t /= 10.0;
+            x = (x + t) * f;
+            return *this;
+        }
+        Reader& operator>>(__float128& x)
+        {
+            x = 0;
+            __float128 t = 0;
+            short f = 1, s = 0;
+            char c = getchar();
+            while ((c < '0' || c > '9') && c != '.')
+            {
+                if (c == '-')
+                    f *= -1;
+                c = getchar();
+            }
+            while (c >= '0' && c <= '9' && c != '.')
+                x = x * 10 + (c ^ 48), c = getchar();
+            if (c == '.')
+                c = getchar();
+            else
+            {
+                x *= f;
+                return *this;
+            }
+            while (c >= '0' && c <= '9')
+                t = t * 10 + (c ^ 48), s++, c = getchar();
+            while (s--)
+                t /= 10.0;
+            x = (x + t) * f;
+            return *this;
+        }
+        Reader& operator>>(char& c)
+        {
+            c = getchar();
+            while (c == ' ' || c == '\n' || c == '\r')
+                c = getchar();
+            return *this;
+        }
+        Reader& operator>>(char* str)
+        {
+            int len = 0;
+            char c = getchar();
+            while (c == ' ' || c == '\n' || c == '\r')
+                c = getchar();
+            while (c != ' ' && c != '\n' && c != '\r')
+                str[len++] = c, c = getchar();
+            str[len] = '\0';
+            return *this;
+        }
+        Reader& operator>>(string& str)
+        {
+            str.clear();
+            char c = getchar();
+            while (c == ' ' || c == '\n' || c == '\r')
+                c = getchar();
+            while (c != ' ' && c != '\n' && c != '\r')
+                str.push_back(c), c = getchar();
+            return *this;
+        }
+        Reader() {}
+} cin;
+const char endl = '\n';
+struct Writer
+{
+        const int Setprecision = 6;
+        typedef int mxdouble;
+        template <typename T>
+        Writer& operator<<(T x)
+        {
+            if (x == 0)
+            {
+                putchar('0');
+                return *this;
+            }
+            if (x < 0)
+                putchar('-'), x = -x;
+            static short sta[40];
+            short top = 0;
+            while (x > 0)
+                sta[++top] = x % 10, x /= 10;
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            return *this;
+        }
+        Writer& operator<<(double x)
+        {
+            if (x < 0)
+                putchar('-'), x = -x;
+            mxdouble _ = x;
+            x -= (double)_;
+            static short sta[40];
+            short top = 0;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            if (top == 0)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            putchar('.');
+            for (int i = 0; i < Setprecision; i++)
+                x *= 10;
+            _ = x;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            for (int i = 0; i < Setprecision - top; i++)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            return *this;
+        }
+        Writer& operator<<(long double x)
+        {
+            if (x < 0)
+                putchar('-'), x = -x;
+            mxdouble _ = x;
+            x -= (long double)_;
+            static short sta[40];
+            short top = 0;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            if (top == 0)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            putchar('.');
+            for (int i = 0; i < Setprecision; i++)
+                x *= 10;
+            _ = x;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            for (int i = 0; i < Setprecision - top; i++)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            return *this;
+        }
+        Writer& operator<<(__float128 x)
+        {
+            if (x < 0)
+                putchar('-'), x = -x;
+            mxdouble _ = x;
+            x -= (__float128)_;
+            static short sta[40];
+            short top = 0;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            if (top == 0)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            putchar('.');
+            for (int i = 0; i < Setprecision; i++)
+                x *= 10;
+            _ = x;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            for (int i = 0; i < Setprecision - top; i++)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            return *this;
+        }
+        Writer& operator<<(char c)
+        {
+            putchar(c);
+            return *this;
+        }
+        Writer& operator<<(char* str)
+        {
+            int cur = 0;
+            while (str[cur])
+                putchar(str[cur++]);
+            return *this;
+        }
+        Writer& operator<<(const char* str)
+        {
+            int cur = 0;
+            while (str[cur])
+                putchar(str[cur++]);
+            return *this;
+        }
+        Writer& operator<<(string str)
+        {
+            int st = 0, ed = str.size();
+            while (st < ed)
+                putchar(str[st++]);
+            return *this;
+        }
+        Writer() {}
+} cout;
+}  // namespace Fastio
+using namespace Fastio;
+#define cin Fastio::cin
+#define cout Fastio::cout
+#define endl Fastio::endl
+
+namespace slove
+{
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+#define MOD 998244353
+
+vector<ll> q, w;
+
+ll quick_mod_pow(ll a, ll s, ll d)
+{
+    ll res = 1;
+    a %= d;
+    while (s)
+    {
+        if (s & 1)
+            res = (res * a) % d;
+        s >>= 1;
+        a = (a * a) % d;
+    }
+    return res;
+}
+
+inline void solve()
+{
+    q.clear();
+    w.clear();
+    ll n;
+    cin >> n;
+    q.reserve(n + 1);
+    w.reserve(n + 1);
+    q[n] = w[n] = -1;
+    ll qm = n, wm = n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> q[i];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> w[i];
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        if (q[i] > q[qm])
+            qm = i;
+        if (w[i] > w[wm])
+            wm = i;
+        ll iq, iw;
+        if (q[qm] > w[wm])
+        {
+            iq = qm;
+            iw = i - qm;
+        }
+        else if (q[qm] < w[wm])
+        {
+            iq = i - wm;
+            iw = wm;
+        }
+        else
+        {
+            if (q[i - wm] < w[i - qm])
+            {
+                iq = qm;
+                iw = i - qm;
+            }
+            else
+            {
+                iq = i - wm;
+                iw = wm;
+            }
+        }
+        cout << (quick_mod_pow(2, q[iq], MOD) + quick_mod_pow(2, w[iw], MOD)) % MOD << " \n"[i == n - 1];
+    }
+}
+}  // namespace slove
+
+signed main()
+{
+    // fast;
+    ll T = 1;
+    // T = read();
+    cin >> T;
+    while (T--)
+        slove::solve();
+    return 0;
+}
---
+++ codeforce/2116/C.cpp
@@ -0,0 +1,460 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define endl "\n"
+#define fast                                                                                                           \
+    ios::sync_with_stdio(false);                                                                                       \
+    ios_base::sync_with_stdio(0);                                                                                      \
+    cin.tie(0);                                                                                                        \
+    cout.tie(0);
+#define gc() getchar()
+#define MOD 100000000
+#define one void(cout << "-1\n")
+#define pb(c) push_back(c)
+#define pf(c) push_front(c)
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define rint register int
+#define vi vector<int>
+#define vll vector<long long>
+#define fi first
+#define se second
+#define all(c) (c).begin(), (c).end()
+typedef long long ll;
+typedef unsigned long long ull;
+
+namespace Fread
+{
+const int SIZE = 1 << 16;
+char buf[SIZE], *S, *T;
+inline char getchar()
+{
+    if (S == T)
+    {
+        T = (S = buf) + fread(buf, 1, SIZE, stdin);
+        if (S == T)
+            return '\n';
+    }
+    return *S++;
+}
+}  // namespace Fread
+using namespace Fread;
+namespace Fwrite
+{
+const int SIZE = 1 << 16;
+char buf[SIZE], *S = buf, *T = buf + SIZE;
+inline void flush()
+{
+    fwrite(buf, 1, S - buf, stdout);
+    S = buf;
+}
+inline void putchar(char c)
+{
+    *S++ = c;
+    if (S == T)
+        flush();
+}
+struct NTR
+{
+        ~NTR() { flush(); }
+} ztr;
+}  // namespace Fwrite
+using namespace Fwrite;
+#define getchar Fread::getchar
+#define putchar Fwrite::putchar
+namespace Fastio
+{
+struct Reader
+{
+        template <typename T>
+        Reader& operator>>(T& x)
+        {
+            x = 0;
+            short f = 1;
+            char c = getchar();
+            while (c < '0' || c > '9')
+            {
+                if (c == '-')
+                    f *= -1;
+                c = getchar();
+            }
+            while (c >= '0' && c <= '9')
+                x = (x << 3) + (x << 1) + (c ^ 48), c = getchar();
+            x *= f;
+            return *this;
+        }
+        Reader& operator>>(double& x)
+        {
+            x = 0;
+            double t = 0;
+            short f = 1, s = 0;
+            char c = getchar();
+            while ((c < '0' || c > '9') && c != '.')
+            {
+                if (c == '-')
+                    f *= -1;
+                c = getchar();
+            }
+            while (c >= '0' && c <= '9' && c != '.')
+                x = x * 10 + (c ^ 48), c = getchar();
+            if (c == '.')
+                c = getchar();
+            else
+            {
+                x *= f;
+                return *this;
+            }
+            while (c >= '0' && c <= '9')
+                t = t * 10 + (c ^ 48), s++, c = getchar();
+            while (s--)
+                t /= 10.0;
+            x = (x + t) * f;
+            return *this;
+        }
+        Reader& operator>>(long double& x)
+        {
+            x = 0;
+            long double t = 0;
+            short f = 1, s = 0;
+            char c = getchar();
+            while ((c < '0' || c > '9') && c != '.')
+            {
+                if (c == '-')
+                    f *= -1;
+                c = getchar();
+            }
+            while (c >= '0' && c <= '9' && c != '.')
+                x = x * 10 + (c ^ 48), c = getchar();
+            if (c == '.')
+                c = getchar();
+            else
+            {
+                x *= f;
+                return *this;
+            }
+            while (c >= '0' && c <= '9')
+                t = t * 10 + (c ^ 48), s++, c = getchar();
+            while (s--)
+                t /= 10.0;
+            x = (x + t) * f;
+            return *this;
+        }
+        Reader& operator>>(__float128& x)
+        {
+            x = 0;
+            __float128 t = 0;
+            short f = 1, s = 0;
+            char c = getchar();
+            while ((c < '0' || c > '9') && c != '.')
+            {
+                if (c == '-')
+                    f *= -1;
+                c = getchar();
+            }
+            while (c >= '0' && c <= '9' && c != '.')
+                x = x * 10 + (c ^ 48), c = getchar();
+            if (c == '.')
+                c = getchar();
+            else
+            {
+                x *= f;
+                return *this;
+            }
+            while (c >= '0' && c <= '9')
+                t = t * 10 + (c ^ 48), s++, c = getchar();
+            while (s--)
+                t /= 10.0;
+            x = (x + t) * f;
+            return *this;
+        }
+        Reader& operator>>(char& c)
+        {
+            c = getchar();
+            while (c == ' ' || c == '\n' || c == '\r')
+                c = getchar();
+            return *this;
+        }
+        Reader& operator>>(char* str)
+        {
+            int len = 0;
+            char c = getchar();
+            while (c == ' ' || c == '\n' || c == '\r')
+                c = getchar();
+            while (c != ' ' && c != '\n' && c != '\r')
+                str[len++] = c, c = getchar();
+            str[len] = '\0';
+            return *this;
+        }
+        Reader& operator>>(string& str)
+        {
+            str.clear();
+            char c = getchar();
+            while (c == ' ' || c == '\n' || c == '\r')
+                c = getchar();
+            while (c != ' ' && c != '\n' && c != '\r')
+                str.push_back(c), c = getchar();
+            return *this;
+        }
+        Reader() {}
+} cin;
+const char endl = '\n';
+struct Writer
+{
+        const int Setprecision = 6;
+        typedef int mxdouble;
+        template <typename T>
+        Writer& operator<<(T x)
+        {
+            if (x == 0)
+            {
+                putchar('0');
+                return *this;
+            }
+            if (x < 0)
+                putchar('-'), x = -x;
+            static short sta[40];
+            short top = 0;
+            while (x > 0)
+                sta[++top] = x % 10, x /= 10;
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            return *this;
+        }
+        Writer& operator<<(double x)
+        {
+            if (x < 0)
+                putchar('-'), x = -x;
+            mxdouble _ = x;
+            x -= (double)_;
+            static short sta[40];
+            short top = 0;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            if (top == 0)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            putchar('.');
+            for (int i = 0; i < Setprecision; i++)
+                x *= 10;
+            _ = x;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            for (int i = 0; i < Setprecision - top; i++)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            return *this;
+        }
+        Writer& operator<<(long double x)
+        {
+            if (x < 0)
+                putchar('-'), x = -x;
+            mxdouble _ = x;
+            x -= (long double)_;
+            static short sta[40];
+            short top = 0;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            if (top == 0)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            putchar('.');
+            for (int i = 0; i < Setprecision; i++)
+                x *= 10;
+            _ = x;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            for (int i = 0; i < Setprecision - top; i++)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            return *this;
+        }
+        Writer& operator<<(__float128 x)
+        {
+            if (x < 0)
+                putchar('-'), x = -x;
+            mxdouble _ = x;
+            x -= (__float128)_;
+            static short sta[40];
+            short top = 0;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            if (top == 0)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            putchar('.');
+            for (int i = 0; i < Setprecision; i++)
+                x *= 10;
+            _ = x;
+            while (_ > 0)
+                sta[++top] = _ % 10, _ /= 10;
+            for (int i = 0; i < Setprecision - top; i++)
+                putchar('0');
+            while (top > 0)
+                putchar(sta[top] + '0'), top--;
+            return *this;
+        }
+        Writer& operator<<(char c)
+        {
+            putchar(c);
+            return *this;
+        }
+        Writer& operator<<(char* str)
+        {
+            int cur = 0;
+            while (str[cur])
+                putchar(str[cur++]);
+            return *this;
+        }
+        Writer& operator<<(const char* str)
+        {
+            int cur = 0;
+            while (str[cur])
+                putchar(str[cur++]);
+            return *this;
+        }
+        Writer& operator<<(string str)
+        {
+            int st = 0, ed = str.size();
+            while (st < ed)
+                putchar(str[st++]);
+            return *this;
+        }
+        Writer() {}
+} cout;
+}  // namespace Fastio
+/*
+using namespace Fastio;
+#define cin Fastio::cin
+#define cout Fastio::cout
+#define endl Fastio::endl*/
+
+namespace slove
+{
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+const int max1 = 5000;
+vi mu(5005, 0);
+
+void init()
+{
+    vi primes;
+    vector<bool> is_comp(5005, false);
+    mu[1] = 1;
+    for (int i = 2; i <= max1; i++)
+    {
+        if (!is_comp[i])
+        {
+            primes.push_back(i);
+            mu[i] = -1;
+        }
+        for (int p : primes)
+        {
+            int x = i * p;
+            if (x > max1)
+                break;
+            is_comp[x] = true;
+            if (i % p == 0)
+            {
+                mu[x] = 0;
+                break;
+            }
+            else
+            {
+                mu[x] = -mu[i];
+            }
+        }
+    }
+}
+
+inline void solve()
+{
+    ll n;
+    cin >> n;
+    vll nums(n);
+    cin >> nums[0];
+    ll tg = nums[0];  //: target gcd
+    ll odd = 0, even = 0;
+
+    for (ll i = 1; i < n; ++i)
+    {
+        cin >> nums[i];
+        tg = gcd(tg, nums[i]);
+        if (nums[i] & 1)
+            odd = nums[i];
+        else
+            even = nums[i];
+    }
+    if (n == 1)
+    {
+        cout << "0\n";
+        return;
+    }
+
+    ll etg = 0;  //: equal target gcd
+    for (int i = 0; i < n; ++i)
+        if (nums[i] == tg)
+            ++etg;
+    if (etg)
+    {
+        cout << n - etg << endl;
+        return;
+    }
+
+    vi di(n);
+    int m = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        di[i] = nums[i] / tg;
+        m = max(m, di[i]);
+    }
+    vi fre(m + 1);
+    for (int d : di)
+        fre[d]++;
+
+    vi ctDiv(m + 1, 0);
+    for (int d = 1; d <= m; ++d)
+        for (int x = d; x <= m; x += d)
+            ctDiv[d] += fre[x];
+
+    ll k = -1, upK = min(n, 1ll * 5);
+    for (int kk = 2; kk < upK; ++kk)
+    {
+        ll total = 0;
+        for (int d = 1; d <= m; ++d)
+        {
+            int cnt = ctDiv[d];
+            if (cnt < kk)
+                continue;
+            ll c = 1;
+            for (int i = 1; i <= kk; ++i)
+            {
+                c *= (cnt - kk + i) / i;
+            }
+            total += c * mu[d];
+        }
+        if (total > 0)
+        {
+            k = kk;
+            break;
+        }
+    }
+}
+}  // namespace slove
+
+signed main()
+{
+    slove::init();
+    // fast;
+    ll T = 1;
+    // T = read();
+    cin >> T;
+    while (T--)
+        slove::solve();
+    return 0;
+}
